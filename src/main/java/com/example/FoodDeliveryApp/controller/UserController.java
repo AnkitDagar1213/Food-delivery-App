1d0ee137d677620bbf4f9b468fe69e315e3a127e
Ankit Dagar <ankit.dagar1213@gmail.com>
2023-03-15 19:26:42 +0530

    Initial commit

--- src/main/java/com/example/FoodDeliveryApp/controller/UserController.java
+++ src/main/java/com/example/FoodDeliveryApp/controller/UserController.java
@@ -1,9 +1,33 @@
 package com.example.FoodDeliveryApp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.FoodDeliveryApp.model.User;
+import com.example.FoodDeliveryApp.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/FoodDeliver-App")
 public class UserController {
-
+    @Autowired
+    UserService userservice;
+    @PostMapping(value = "/create_user")
+    public ResponseEntity<String> create_user(@Valid @RequestBody User user){
+        User user1=userservice.create_user(user);
+        return new ResponseEntity<>(user1+"User created", HttpStatus.ACCEPTED);
+    }
+    @GetMapping(value = "/get_all_Users")
+    public ResponseEntity<String> get_all_Users(){
+        List<User> user=userservice.get_all_Users();
+        return new ResponseEntity<>(user+"All Users",HttpStatus.ACCEPTED);
+    }
+    @GetMapping(value = "/get_User_by_Id")
+    public ResponseEntity<String> get_User_by_Id(@RequestParam Integer Id){
+        User user=userservice.get_User_by_Id(Id);
+        return new ResponseEntity<>(user+"Users by "+Id,HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/example/FoodDeliveryApp/service/UserService.java
+++ src/main/java/com/example/FoodDeliveryApp/service/UserService.java
@@ -1,6 +1,22 @@
 package com.example.FoodDeliveryApp.service;
 
+import com.example.FoodDeliveryApp.model.User;
+import com.example.FoodDeliveryApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 @Service
 public class UserService {
+    @Autowired
+    UserRepository userRepository;
+    public User create_user(User user) {
+        return userRepository.save(user);
+    }
+    public List<User> get_all_Users() {
+        return userRepository.findAll();
+    }
+
+    public User get_User_by_Id(Integer Id) {
+        return userRepository.findById(Id).get();
+    }
 }
